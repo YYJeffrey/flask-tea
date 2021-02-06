8b1c9920637007d38ee09fcd81e9ae94e35f962b
YYJeffrey <501934027@qq.com>
2021-02-06 23:07:03 +0800

    feat: 新增枚举模块

--- app/lib/enums.py
+++ app/lib/enums.py
@@ -3,3 +3,10 @@
     :copyright: (c) 2021 by Jeffrey.
     :license: MIT, see LICENSE for more details.
 """
+from enum import Enum
+
+
+class GenderEnum(Enum):
+    UN_KNOW = 0
+    MAN = 1
+    WOMAN = 2
